1b137c6a925d543add2bd7304a57086796d61026
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-19 17:15:39 +0200

    Funtion that sum items lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function sum_list which takes a list
+and return its sum."""
+
+
+def sum_list(input_list: list) -> float:
+    """ Returns the sum of list"""
+    return sum(input_list)
