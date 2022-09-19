2b46203e156bdadc1e134321fac1aa3da3faefd6
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-19 16:27:02 +0200

    Contact two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+add that takes a string a and  string
+b as arguments and returns their contact as a string.
+"""
+
+
+def contact(str1: str, str: str) -> str:
+    """
+    Return the contcat of two string
+    """
+    return str1 + str2
