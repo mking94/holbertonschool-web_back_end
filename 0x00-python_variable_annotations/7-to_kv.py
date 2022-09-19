73707ac0e6e0e8aeba58b36474a4fd88ad003e3a
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-19 17:42:47 +0200

    Returns a Tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Convert to tuple"""
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
 
 def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
