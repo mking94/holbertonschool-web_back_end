b72a3bce34f34b30b8587e887d79231d426834d5
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-19 14:17:06 +0200

    Ttype-annotated function add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,5 +2,5 @@
 """ Type-annotated function add that takes a float a and a float b as
 arguments and returns their sum as a float """
 
-def add(a, b):
+def add(a: float, b: float) -> float:
     return a + b
