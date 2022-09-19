ea8670811ddec677a523bbf50f2d02b30ea81d51
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-19 17:03:06 +0200

    Convert float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function floor which
+takes a float n as argument and returns the floor of the float."""
+
+
+def to_str(n: float) -> str:
+    """ Returns the floor of the float"""
+    return str(n)
