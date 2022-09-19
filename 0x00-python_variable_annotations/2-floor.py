300802b612c934ca3c410bcae827fb66e3c28b9f
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-19 16:54:44 +0200

    Floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Type-annotated function floor which 
+takes a float n as argument and returns the floor of the float."""
+def floor(n: float) -> float:
+    """ Returns the floor of the float"""
+    return float(str(n)[0:str(n).index(".")])
