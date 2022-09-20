5a5092e0de180f5ac1f4dc8d7c6260be6aa89ef4
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-20 17:29:32 +0200

    Coroutine that takes in an integer argument and return random

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 ''' Description: asynchronous coroutine that takes in an integer argument
-                 (max_delay, with a default value of 10) named wait_random
-                 that waits for a random delay between 0 and max_delay
-                 (included and float value) seconds and eventually returns it
-    Arguments: max_delay: int = 10
+                 (max_delay, with a default value of 10)
 '''
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ return a ramdom number after wait"""
+    """ Return a ramdom number after wait"""
     delay = max_delay * random.random()
     await asyncio.sleep(delay)
     return delay
