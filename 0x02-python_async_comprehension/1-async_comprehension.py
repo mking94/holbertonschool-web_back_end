2b0b9f992ecef79ad22c92d2829ee13991f1b93c
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-21 10:36:14 +0200

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Async Comprehensions """
+import asyncio 
+import random
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension():
+    """ Collect 10 random numbers using an async
+    comprehensing over async_generator """
+    random_list = [i async for i in async_generator()]
+    return random_list
