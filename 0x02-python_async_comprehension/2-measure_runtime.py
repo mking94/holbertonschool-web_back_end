69ff98953acf4642e510c74b2efee3b501b62331
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-21 10:50:04 +0200

    Run time for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Run time for four parallel comprehensions """
+import asyncio 
+
+async_comprehension = __import__('1-async_comprehension.py').async_comprehension
+
+async def async_comprehension():
+    """ Collect 10 random numbers using an async
+    comprehensing over async_generator """
+    for i in range(4):
+      asyncio.gather(async_comprehension)
