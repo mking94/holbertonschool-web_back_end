5686cd9f76143aa6dbf84ad8dd9ec5392ab312bf
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-23 12:39:48 +0200

    Basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-""" BasicCache """
+""" Python caching systems """
 
-BaseCaching = __import__('BaseCaching')
+from base_caching import BaseCaching
+
 
 class BasicCache(BaseCaching):
-    """ BasicCache module """
+    """ Dict caching system """
+
     def put(self, key, item):
-        """ Put the key and item"""
-        if (key != None or item != None):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
+
     def get(self, key):
-        """ Get the cache data"""
-        if key is not None and key not in self.cache_data:
-            return self.cache_data[key]
-        return None
+        """ Get an item by key """
+        return self.cache_data.get(key)
