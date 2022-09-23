fbd61a76e911b5936c21c19f08d0888e83889efa
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-23 13:00:32 +0200

    LIFO Caching

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -4,24 +4,39 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """
+    This class will inherit self.cache_data from BaseCashing
+    """
     def __init__(self):
+        """
+        Init from BaseCaching
+        """
         super().__init__()
-        self.last_key = ''
 
     def put(self, key, item):
-        """ Assign to the dictionary, LIFO algorithm, add element """
-        if key and item:
+        """
+        Adds key value pairs to self.cache_data
+        """
+        if key is not None and item is not None:
+            keyList = list(self.cache_data)[0:]
+            if key in keyList:
+                del self.cache_data[key]
+                keyList.remove(key)
+            if (len(keyList) + 1) > BaseCaching.MAX_ITEMS:
+                remove = keyList.pop()
+                print("DISCARD: " + remove)
+                del self.cache_data[remove]
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                print("DISCARD: {}".format(self.last_key))
-                self.cache_data.pop(self.last_key)
-            self.last_key = key
+            keyList.append(key)
+
+        else:
+            pass
 
     def get(self, key):
-        """ Return the value linked """
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        if key in self.cache_data:
-            value = self.cache_data[key]
-            return 
+        """
+        Retrieves items from self.cache_data by key
+        """
+        try:
+            return self.cache_data[key]
+        except Exception:
+            pass
