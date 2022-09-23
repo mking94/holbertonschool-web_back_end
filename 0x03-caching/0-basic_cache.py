d9e436077dc61be7dc86d15c73c1da2df930ffb1
mking94 <dadimaher94@gmail.com>
2022-09-23 10:03:23 +0000

    Basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -4,15 +4,20 @@
 
 BaseCaching = __import__('BaseCaching').BaseCaching
 
+
 class BasicCache(BaseCaching):
+
+
 """ BasicCache module """
-    def put(self, key, item):
+
+   def put(self, key, item):
         """ Put the key and item"""
-        if (key != None or item != None):
+        if (key is not None or item is not None):
             self.cache_data[key] = item
+
     def get(self, key):
         """ Get the cache data"""
         if (key is None or key not in self.cache_data.keys()):
             return None
-        else :
+        else:
             return self.cache_data[key]
