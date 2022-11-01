dbd80ca01f95c2768f0d15878643da59cb34d673
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-11-01 08:51:55 +0100

    Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Tasks - Redis """
+
+import redis
+
+class Cache:
+    """ Class for implementing a Cache """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the input data in Redis using a
+        random key and return the key.
+        """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
