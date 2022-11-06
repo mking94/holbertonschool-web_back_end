e5a96b4f213cb3298141809a8c4a730d732f98fc
mking94 <dadimaher94@gmail.com>
2022-11-06 11:54:21 +0000

    SQL script that creates a table users

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Script can be executed on any database
+CREATE TABLE If NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM ('US','CO','TN') DEFAULT 'US',
+    PRIMARY KEY(id)
+);
