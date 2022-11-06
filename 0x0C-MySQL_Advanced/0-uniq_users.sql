0c07510286c5194640f1181c6e98c50ffa4066d5
mking94 <dadimaher94@gmail.com>
2022-11-06 11:46:33 +0000

    Script can be executed on any database

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Script can be executed on any database
-CREATE TABLE If NOT EXISTS `users` (  
+CREATE TABLE If NOT EXISTS `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
+    `name` VARCHAR(255),
     PRIMARY KEY(id)
 );
