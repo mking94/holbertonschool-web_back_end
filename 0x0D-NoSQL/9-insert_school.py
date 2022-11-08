1fcae8f73cd977d6e73da8487fc658035fc8bd61
mking94 <dadimaher94@gmail.com>
2022-11-08 01:29:01 -0800

    Insert a document in Python

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that insert documents in a collection """
+
+def insert_school(mongo_collection, **kwargs):
+    """ Function that insert documents in a collection """
+    if mongo_collection is None:
+        return None
+    return mongo_collection.insert_one().inserted_id
