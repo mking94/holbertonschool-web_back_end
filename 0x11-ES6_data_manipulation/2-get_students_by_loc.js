f572a96f7acd54bc995b6ba27952761acaeb5437
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-11-22 09:28:57 +0100

    Filter

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(arrayobjects, loc) {
   if (!(Array.isArray(arrayobjects))) {
     return [];
   }
-  return arrayobjects.filter(arrayobjects => arrayobjects.location == loc);
+  return arrayobjects.filter((arrayobjects) => arrayobjects.location == loc);
 }
