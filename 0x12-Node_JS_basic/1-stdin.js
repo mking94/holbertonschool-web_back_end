aaf6085cb59052f2da2adccc646f017ffe711051
mking94 <dadimaher94@gmail.com>
2022-11-27 08:38:58 -0800

    Using Process stdin

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
 
-  if ((chunk) !== null) {
+  if ((input) !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
